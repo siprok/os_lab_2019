b90f42dee4d519d5604888fa9bfba73d2f8e72c3
siprok <siprokudin1@mail.ru>
2020-03-13 19:15:21 +0000

    lab2 tasks 1,2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	char buf;
+	size_t str_length = strlen(str);
+	size_t half_length = str_length >> 1;
+	for(int i = 0; i < half_length; i++)
+	{
+		buf = str[i];
+		str[i] = str[str_length - 1 - i];
+		str[str_length - 1 - i] = buf;
+	}
 }
 
